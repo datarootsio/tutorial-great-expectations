271c1f3e11e95c4c0f712c0afb532677a9966fbb
Ilion Beyst <ilion@dataroots.io>
2021-01-11 17:31:43 +0100

    fix volume label

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build . -t tutorial-great_expectations
-docker run -it --rm -p 8888:8888 -v "$PWD":/home/jovyan/work tutorial-great_expectations
+docker run -it --rm -p 8888:8888 -v "$PWD":/home/jovyan/work:Z tutorial-great_expectations
